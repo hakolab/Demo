07746c74be9c0895c53fba335a70103471d1e7d6
はこだてたろう <hakodate.1106v@gmail.com>
2021-07-04 18:04:54 +0900

    小節線の動的変更

--- src/AppData.js
+++ src/AppData.js
@@ -1,9 +1,54 @@
-const config = {
-  octave: 1,
-  noteCount: 8
-};
+const beatOptions = [
+  {
+    key: 'two-four',
+    value: '2/4'
+  },
+  {
+    key: 'three-four',
+    value: '3/4'
+  },
+  {
+    key: 'four-four',
+    value: '4/4'
+  },
+  {
+    key: 'six-eight',
+    value: '6/8'
+  }
+];
 
-export const toyPiano = {
+const oneOctave = {
+  mode: "oneOctave",
+  data: [
+    {
+      octave: 5,
+      bKeyIndex: [],
+      tones: [
+        { pitchName: "C" }
+      ]
+    },
+    {
+      octave: 4,
+      bKeyIndex: [1, 3, 5, 8, 10],
+      tones: [
+        { pitchName: "B" },
+        { pitchName: "Bb" },
+        { pitchName: "A" },
+        { pitchName: "Ab" },
+        { pitchName: "G" },
+        { pitchName: "Gb" },
+        { pitchName: "F" },
+        { pitchName: "E" },
+        { pitchName: "Eb" },
+        { pitchName: "D" },
+        { pitchName: "Db" },
+        { pitchName: "C" }
+      ]
+    }
+  ]
+}
+
+const toyPiano = {
   mode: "toyPiano",
   data: [
   {
@@ -65,38 +110,7 @@ export const toyPiano = {
 ]
 }
 
-export const oneOctave = {
-  mode: "oneOctave",
-  data: [
-    {
-      octave: 5,
-      bKeyIndex: [],
-      tones: [
-        { pitchName: "C" }
-      ]
-    },
-    {
-      octave: 4,
-      bKeyIndex: [1, 3, 5, 8, 10],
-      tones: [
-        { pitchName: "B" },
-        { pitchName: "Bb" },
-        { pitchName: "A" },
-        { pitchName: "Ab" },
-        { pitchName: "G" },
-        { pitchName: "Gb" },
-        { pitchName: "F" },
-        { pitchName: "E" },
-        { pitchName: "Eb" },
-        { pitchName: "D" },
-        { pitchName: "Db" },
-        { pitchName: "C" }
-      ]
-    }
-  ]
-}
-
-export const keyboard76 = {
+const keyboard76 = {
   mode: "keyboard76",
   data: [
     {
@@ -219,31 +233,10 @@ export const keyboard76 = {
     },
   ]
 }
-/* const oneOctave = [
-  { pitchName: "B", octave: 4 },
-  { pitchName: "Bb", octave: 4 },
-  { pitchName: "A", octave: 4 },
-  { pitchName: "Ab", octave: 4 },
-  { pitchName: "G", octave: 4 },
-  { pitchName: "Gb", octave: 4 },
-  { pitchName: "F", octave: 4 },
-  { pitchName: "E", octave: 4 },
-  { pitchName: "Eb", octave: 4 },
-  { pitchName: "D", octave: 4 },
-  { pitchName: "Db", octave: 4 },
-  { pitchName: "C", octave: 4 }
-]; */
 
-/* export function getInitialPlayNotes(keyboardType) {
-  console.log(keyboardType);
-  switch (keyboardType) {
-    case "oneOctave":
-      return {
-        type: "oneOctave",
-        notes: oneOctave.map((_) => new Array(config.noteCount).fill(false)),
-        tones: oneOctave
-      };
-    default:
-  }
+export {
+  beatOptions,
+  oneOctave,
+  toyPiano,
+  keyboard76
 }
- */
--- src/Pianoroll.js
+++ src/Pianoroll.js
@@ -1,47 +1,39 @@
 import React, { useState, useEffect } from "react";
 import * as Tone from "tone";
 import { Slider } from "@material-ui/core";
-import { keyboard76, oneOctave, toyPiano } from "./data";
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import * as AppData from "./AppData";
 import './styles.css'
 import './styles.scss'
-
-const config = {
-  octave: 1,
-  noteCount: 8
-};
-
-const steps = new Array(config.noteCount).fill(null).map((_, i) => i);
-
-/*
-コントローラーを作成
-
-動的に鍵盤を表示
-・てっぺんのド
-・鍵盤数を選べるように
-  ８８鍵
-  キーボード（49~61）
-  トイピアノ
-
-全体のレイアウト
-
-*/
-
-const sequencer = new Tone.Sequence();
-
-const bKeyIndex = [1, 3, 5, 8, 10];
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 
 export default function Pianoroll() {
-  const [tones, setTones] = useState(keyboard76);
+  const [noteCount, setNoteCount] = useState(32);
+  const [tones, setTones] = useState(AppData.keyboard76);
   const [notes, setNotes] = useState(
-    tones.data.map(octave => octave.tones.map(tones =>  new Array(config.noteCount).fill(false)))
+    tones.data.map(octaveObj => octaveObj.tones.map(_ =>  new Array(noteCount).fill(false)))
   );
   //console.log(notes)
   const [isPlaying, setIsPlaying] = useState(Tone.Transport.state);
   //console.log(`state is ${isPlaying}`);
   const [currentStep, setCurrentStep] = useState();
-  const steps = new Array(config.noteCount).fill(null).map((_, i) => i);
+  const steps = new Array(noteCount).fill(null).map((_, i) => i);
   const [bpm, setBpm] = useState(Tone.Transport.bpm.value);
   const [isChanging, setIsChanging] = useState(false);
+  const [bars, setBars] = useState(4);
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   function handleMouseDown(event, octave, row, col) {
     // 要素をドラッグしようとするのを防ぐ
@@ -49,13 +41,13 @@ export default function Pianoroll() {
 
     console.log(`mouse down on ${row}:${col} of ${octave}`);
 
-    const newNotes = notes.slice()//JSON.parse(JSON.stringify(notes));
+    const newNotes = notes.slice();
     const current = newNotes[octave][row][col];
     newNotes[octave][row][col] = !current;
     setNotes(newNotes);
   }
 
-  function handleMouseEnter(event, octave, row, col) {
+  function handleMouseEnter(event, octaveIndex, toneIndex, noteIndex) {
     //console.log(`mouse enter on ${row}:${col}`);
     // 左クリックされていなければ return
     if (event.buttons !== 1) {
@@ -68,9 +60,9 @@ export default function Pianoroll() {
     }
 
     event.preventDefault();
-    const newNotes = notes.slice()//JSON.parse(JSON.stringify(notes));
-    const current = newNotes[octave][row][col];
-    newNotes[octave][row][col] = !current;
+    const newNotes = notes.slice();
+    const current = newNotes[octaveIndex][toneIndex][noteIndex];
+    newNotes[octaveIndex][toneIndex][noteIndex] = !current;
     setNotes(newNotes);
   }
 
@@ -81,7 +73,7 @@ export default function Pianoroll() {
   },[Tone.Transport.state])
 
   function start() {
-    const synth = new Tone.PolySynth().toDestination();
+    const synth = new Tone.PolySynth().toDestination()
     const seq = new Tone.Sequence((time, step) => {
       const playNotes = getPlayNotes(step);
       synth.triggerAttackRelease(playNotes, "8t", time);
@@ -107,122 +99,204 @@ export default function Pianoroll() {
 
   function getPlayNotes(step) {
     let playNotes = [];
-
-    notes.forEach((octave, octIndex) => {
-      octave.forEach((notes, rowIndex) => {
-        if(notes[step]){
-          //console.log(notes[step])
-          //console.log(tones.data[octIndex].tones[step]);
-          //console.log(`${tones.data[octIndex].tones[rowIndex].pitchName}${tones.data[octIndex].octave}`);
-          playNotes.push(`${tones.data[octIndex].tones[rowIndex].pitchName}${tones.data[octIndex].octave}`);
+    notes.forEach((octave, octaveIndex) => {
+      octave.forEach((tone, toneIndex) => {
+        if(tone[step]){
+          playNotes.push(`${tones.data[octaveIndex].tones[toneIndex].pitchName}${tones.data[octaveIndex].octave}`);
         }
       })
-    })
-
-/*     notes.forEach((octave, index) => octave.forEach(data => {
-      console.log(data[step])
-      if (data[step]) {
-        //console.log(`${tones[index].pitchName}${tones[index].octave}`);
-        //playNotes.push(`${tones[index].pitchName}${tones[index].octave}`);
-      }
-    })); */
-
+    });
     return playNotes;
   }
-
+  /** time signature */
   function handleChange(event, newValue) {
     setBpm(newValue);
     Tone.Transport.bpm.value = newValue;
   }
 
+  function handleChangeBars(event, newValue) {
+    setBars(newValue);
+  }
+
   function reset() {
     stop();
     setNotes(
-      tones.data.map(octave => octave.tones.map(tones =>  new Array(config.noteCount).fill(false)))
+      tones.data.map(octaveObj => octaveObj.tones.map(_ =>  new Array(noteCount).fill(false)))
     );
     setBpm(120);
 
     Tone.Transport.bpm.value = 120;
   }
 
+  /* const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  }; */
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+  /** time signature end */
+
   return (
-    <div id="piano-roll">
-      <div id="controller">
-        {
-          isPlaying === "stopped"
-          ? <button type="button" style={{ margin: "5px" }} onClick={start}>
-              start
-            </button>
-          : <button id="stop" type="button" style={{ margin: "5px" }} onClick={stop}>
-              stop
-            </button>
-        }
-        
-        
-        <button id="reset" type="button" style={{ margin: "5px" }} onClick={reset}>
-          reset
-        </button>
-        <div style={{ width: "200px", margin: "auto" }}>
-          <Slider
-            value={bpm}
-            onChange={handleChange}
-            min={40}
-            max={200}
-            onMouseDown={() => setIsChanging(true)}
-            onChangeCommitted={() => setIsChanging(false)}
-          />
-          {bpm}
+    <div id="container">
+      <div id="controller" className="clearfix">
+        <div id="select-buttons">
+          <ButtonGroup color="primary" aria-label="outlined primary button group">
+            <Button onClick={() => setTones(AppData.oneOctave)}>one octave</Button>
+            <Button onClick={() => setTones(AppData.toyPiano)}>toy piano</Button>
+            <Button onClick={() => setTones(AppData.keyboard76)}>keyboard 76</Button>
+          </ButtonGroup>
+        </div>
+        <div id="control-buttons">
+          <ButtonGroup color="primary" aria-label="outlined primary button group">
+            {
+              isPlaying === "stopped"
+              && <Button type="button" onClick={start} className="btn-w-80">
+                  start
+                </Button>
+            }
+            {
+              isPlaying === "started"
+              && <Button id="stop" type="button" onClick={stop} className="btn-w-80">
+                  stop
+                </Button>
+            }        
+            <Button id="reset" type="button" onClick={reset} className="btn-w-80">
+              reset
+            </Button>
+          </ButtonGroup>
+        </div>
+        <div id="time-signature-buttons">
+          <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button" variant="outlined" disableRipple>
+            <Button>{AppData.beatOptions[selectedIndex].value}</Button>
+            <Button
+              color="primary"
+              size="small"
+              aria-controls={open ? 'split-button-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-label="select merge strategy"
+              aria-haspopup="menu"
+              onClick={handleToggle}
+            >
+              <ArrowDropDownIcon />
+            </Button>
+          </ButtonGroup>
+          <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="split-button-menu">
+                      {AppData.beatOptions.map((option, index) => (
+                        <MenuItem
+                          key={option.key}
+                          //disabled={index === 2}
+                          selected={index === selectedIndex}
+                          onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                          {option.value}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+        <div id="bars">
+          <div className="slider">
+            <Slider
+              value={bars}
+              onChange={handleChangeBars}
+              min={2}
+              max={16}
+              onMouseDown={() => setIsChanging(true)}
+              onChangeCommitted={() => setIsChanging(false)}
+            />
+          </div>
+          <span>{bars} bars</span>
+        </div>
+        <div id="tempo">
+          {/* <div className="label">BPM: {bpm}</div> */}
+          <DirectionsWalkIcon/>
+          <div className="slider">
+            <Slider
+              value={bpm}
+              onChange={handleChange}
+              min={40}
+              max={200}
+              onMouseDown={() => setIsChanging(true)}
+              onChangeCommitted={() => setIsChanging(false)}
+            />
+          </div>
+          <DirectionsRunIcon />
+          <span>{bpm}</span>
         </div>
       </div>
+      <div id="piano-roll">
       {
-        tones.data.map((octave, index) => {
+        tones.data.map((octaveObj, octaveIndex) => {
           return (
             <div className="octave">
               <div className={`keyboard ${tones.mode}`}>
                 {
-                  octave.tones.map((tone, rowIndex) => {
-                    let rowClassName = octave.bKeyIndex.indexOf(rowIndex) >= 0 ? "black-key" : "white-key";
+                  octaveObj.tones.map((tone, toneIndex) => {
+                    let rowClassName = octaveObj.bKeyIndex.indexOf(toneIndex) >= 0 ? "black-key" : "white-key";
                     // 最高音域の場合は .top を設定
-                    if(index === 0){
+                    if(octaveIndex === 0){
                       rowClassName += ' top'
                     }
                     // 最低音域の場合は、 .bottom を設定
-                    if((tones.data.length - 1) === index){
+                    if((tones.data.length - 1) === octaveIndex){
                       rowClassName += ' bottom'
                     }
-                    /* if(octave.tones.length === 1){
-                      rowClassName += ' only'
-                    } */
                     return (
                       <div className={`${rowClassName} ${tone.pitchName}`}></div>
                     )
                   })
                 }
               </div>
-              <div className="grid">
+              <div className={`grid ${AppData.beatOptions[selectedIndex].key}`}>
                 {
-                  octave.tones.map((tone, rowIndex) => {
-                    let rowClassName =　octave.bKeyIndex.indexOf(rowIndex) >= 0 ? "b-key" : "w-key";
+                  octaveObj.tones.map((tone, toneIndex) => {
+                    let rowClassName =octaveObj.bKeyIndex.indexOf(toneIndex) >= 0 ? "b-key" : "w-key";
                     return (
-                      <div className={`row ${rowClassName}`}>
+                      <div className={`row ${rowClassName} ${tone.pitchName}`}>
                         {
-                          notes[index][rowIndex].map((note, colIndex) => {
+                          notes[octaveIndex][toneIndex].map((note, noteIndex) => {
                             // 選択されているか
-                            //console.log(note)
                             let cellClassName = note ? "active" : "";
-
                             // シーケンサーがいまのステップか
-                            if (currentStep === colIndex) {
+                            if (currentStep === noteIndex) {
                               cellClassName = cellClassName + " now";
                             }
                             return (
                               <div
-                                key={`${tone.pitchName}${tone.octave}${colIndex}`}
+                                key={`${tone.pitchName}${tone.octave}${noteIndex}`}
                                 onMouseDown={(event) =>
-                                  handleMouseDown(event, index, rowIndex, colIndex)
+                                  handleMouseDown(event, octaveIndex, toneIndex, noteIndex)
                                 }
                                 onMouseEnter={(event) =>
-                                  handleMouseEnter(event, index, rowIndex, colIndex)
+                                  handleMouseEnter(event, octaveIndex, toneIndex, noteIndex)
                                 }
                                 className={cellClassName}
                               ></div>
@@ -237,79 +311,8 @@ export default function Pianoroll() {
             </div>
           )
         })
-
       }
-
-
-{/*       <div className="top-note">
-        <div className="keyboard">
-          <div className="white-key top"></div>
-        </div>
-        <div className="grid">
-          <div className="row">
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-            <div className="w-key"></div>
-          </div>
-        </div>
       </div>
-      <div className="octave">
-        <div className="keyboard">
-          <div className="white-key"></div>
-          <div className="b-key-common si-flat"></div>
-          <div className="white-key"></div>
-          <div className="b-key-common la-flat"></div>
-          <div className="white-key"></div>
-          <div className="b-key-common so-flat"></div>
-          <div className="white-key"></div>
-          <div className="white-key"></div>
-          <div className="b-key-common mi-flat"></div>
-          <div className="white-key"></div>
-          <div className="b-key-common re-flat"></div>
-          <div className="white-key"></div>
-        </div>
-        <div className="grid">
-          {tones.map((tone, rowIndex) => {
-            const bKeyIndex = [1, 3, 5, 8, 10];
-            let rowClassName =
-              bKeyIndex.indexOf(rowIndex) >= 0 ? "b-key" : "w-key";
-            return (
-              <div
-                className={`row ${rowClassName}`}
-                key={`${tone.pitchName}${tone.octave}`}
-              >
-                {notes[rowIndex].map((note, colIndex) => {
-                  // 選択されているか
-                  let cellClassName = note ? "active" : "";
-
-                  // シーケンサーがいまのステップか
-                  if (currentStep === colIndex) {
-                    cellClassName = cellClassName + " now";
-                  }
-                  //console.log("render");
-                  return (
-                    <div
-                      key={`${tone.pitchName}${tone.octave}${colIndex}`}
-                      onMouseDown={(event) =>
-                        handleMouseDown(event, rowIndex, colIndex)
-                      }
-                      onMouseEnter={(event) =>
-                        handleMouseEnter(event, rowIndex, colIndex)
-                      }
-                      className={cellClassName}
-                    ></div>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </div>
-      </div> */}
     </div>
   );
 }
