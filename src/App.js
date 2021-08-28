a4065fc7885e571e70480175000f4ef2a6b9beb8
はこだてたろう <hakodate.1106v@gmail.com>
2021-08-28 10:29:34 +0900

    Gridテスト前

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Fragment, useState } from 'react';
-import Pianoroll from './Pianoroll'
+import { createMuiTheme, ThemeProvider, makeStylesm, useTheme, useMediaQuery } from '@material-ui/core'
+import { orange } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  breakpoints: {
+    keys: ["xs", "tiny", "sm", "md", "lg", "xl", ],
+    values: {
+      xs: 0,
+      tiny: 430,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    }
+  },
+});
+
+
+
+function CustomDiv(){
+  const _theme = useTheme()
+  console.log(_theme.breakpoints)
+  const match = useMediaQuery(_theme.breakpoints.down('tiny'))
+  //const match = useMediaQuery('(min-width:600px)')
+  return (
+    <div>
+      {match ? "true" : "false"}
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <Fragment>
-      <Pianoroll />
+      <ThemeProvider theme={theme}>
+        <CustomDiv />
+      </ThemeProvider>
     </Fragment>
   );
 }
